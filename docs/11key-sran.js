afe18d20103c83c639bcac51773654bbfe369306
suzme <47318249+suzme@users.noreply.github.com>
2021-03-26 23:29:57 +0900

    いろいろ追加

--- docs/11key-sran.js
+++ docs/11key-sran.js
@@ -1,2 +1,506 @@
 const charts = [
+  {
+    "level": "1",
+    "title": "Still Alive In Love",
+    "difName": "Normal",
+    "artist": "FALL",
+    "maker": "Cah-m",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_Cah-m/?id=StillAliveInLove&key=11"
+  },
+  {
+    "level": "1",
+    "title": "コトバトラボラト",
+    "difName": "Normal☆９",
+    "artist": "daniwell",
+    "maker": "フィア",
+    "scoreId": "1",
+    "url": "https://mfv2.sakura.ne.jp/do/_fia/?id=kotoba&key=11"
+  },
+  {
+    "level": "1",
+    "title": "トエト",
+    "difName": "Remake",
+    "artist": "トラボルタ",
+    "maker": "apoi",
+    "scoreId": "0",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no17_toeto.html"
+  },
+  {
+    "level": "1",
+    "title": "ニョキニョキ",
+    "difName": "Normal",
+    "artist": "とよとみ☆ひでよし＋MAKOTO",
+    "maker": "iso",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_iso/?id=08_nyokinyoki&key=11"
+  },
+  {
+    "level": "2",
+    "title": "A land of everlasting summer",
+    "difName": "Leap",
+    "artist": "三崎 新",
+    "maker": "izkdic",
+    "scoreId": "1",
+    "url": "https://vorhandensein.sakura.ne.jp/danoni/282_ec2summer/"
+  },
+  {
+    "level": "2",
+    "title": "cold planet",
+    "difName": "Normal",
+    "artist": "knot",
+    "maker": "音ノコギリ",
+    "scoreId": "1",
+    "url": "http://otonokogiri.sakura.ne.jp/danoni/029_coldplanet.html"
+  },
+  {
+    "level": "2",
+    "title": "Skyscape",
+    "difName": "Extra",
+    "artist": "Phantasma",
+    "maker": "ティックル",
+    "scoreId": "2",
+    "url": "https://cw7.sakura.ne.jp/danoni/2008/0099_Skyscape.html"
+  },
+  {
+    "level": "2",
+    "title": "北風のメロディ",
+    "difName": "Hard",
+    "artist": "八乙女葦菜",
+    "maker": "fixy",
+    "scoreId": "1",
+    "url": "https://mfv2.sakura.ne.jp/do/_fixy/?dos=0178_kitakaze&key=11"
+  },
+  {
+    "level": "2",
+    "title": "夜螢光 ～ Auld Lang Syne",
+    "difName": "Normal",
+    "artist": "TUMENECO",
+    "maker": "ＫＫＫＫＫ",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_KKKKK/?id=p_wriggle&key=11"
+  },
+  {
+    "level": "3",
+    "title": "白恋華 -hakurenka-",
+    "difName": "Normal",
+    "artist": "ちょろ*",
+    "maker": "夏瀬ツナ",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_guracoro_tsuna/?id=hakurenka&key=11"
+  },
+  {
+    "level": "3",
+    "title": "BAD TRIP (chip-mix)",
+    "difName": "Hard",
+    "artist": "ヒゲドライバー",
+    "maker": "apoi",
+    "scoreId": "0",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no7_bad_trip.html"
+  },
+  {
+    "level": "3",
+    "title": "Noble Dance",
+    "difName": "Extra",
+    "artist": "ことり",
+    "maker": "ぷろろーぐ",
+    "scoreId": "0",
+    "url": "http://pw25.g2.xrea.com/noble-d/noble.html"
+  },
+  {
+    "level": "3",
+    "title": "1%の革命前夜",
+    "difName": "Normal",
+    "artist": "作詞・作曲: 龍崎一 / Losstime Life",
+    "maker": "apoi",
+    "scoreId": "0",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no103_revolution_eve.html"
+  },
+  {
+    "level": "3",
+    "title": "大乱闘！グルメレース",
+    "difName": "Normal",
+    "artist": "BREIS",
+    "maker": "フィア",
+    "scoreId": "2",
+    "url": "https://mfv2.sakura.ne.jp/do/_fia/?id=gurume&key=11"
+  },
+  {
+    "level": "3",
+    "title": "風仁雷仁",
+    "difName": "Normal",
+    "artist": "カラフル・サウンズ・ポート",
+    "maker": "natu",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_natu/?id=hr"
+  },
+  {
+    "level": "4",
+    "title": "Glide",
+    "difName": "Hard",
+    "artist": "Phantasma",
+    "maker": "Ail Trap",
+    "scoreId": "2",
+    "url": "https://mfv2.sakura.ne.jp/do/_Cah-m/?id=glide&key=11"
+  },
+  {
+    "level": "4",
+    "title": "Sky High!",
+    "difName": "Normal",
+    "artist": "四葉かげろう",
+    "maker": "ショウタ",
+    "scoreId": "0",
+    "url": "http://ccsyouta.sakura.ne.jp/danoni/155_skyhigh/skyhigh.html"
+  },
+  {
+    "level": "4",
+    "title": "Springtime",
+    "difName": "Hard",
+    "artist": "Phantasma",
+    "maker": "ぷろろーぐ",
+    "scoreId": "0",
+    "url": "http://pw25.g2.xrea.com/sprtm/"
+  },
+  {
+    "level": "4",
+    "title": "くるくるぽん",
+    "difName": "Very Hard",
+    "artist": "もっぴーさうんど",
+    "maker": "apoi",
+    "scoreId": "0",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no78_klkl_pong.html"
+  },
+  {
+    "level": "4",
+    "title": "展開メガパーセク",
+    "difName": "Very Hard",
+    "artist": "島白",
+    "maker": "apoi",
+    "scoreId": "1",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no3_tenkai.html"
+  },
+  {
+    "level": "5",
+    "title": "Crazy Lazy (radio edit)",
+    "difName": "Normal",
+    "artist": "void",
+    "maker": "fixy",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_fixy/?dos=0175_crr&key=11"
+  },
+  {
+    "level": "5",
+    "title": "Erebus",
+    "difName": "Hard",
+    "artist": "unatra",
+    "maker": "ショウタ",
+    "scoreId": "0",
+    "url": "http://ccsyouta.sakura.ne.jp/danoni/251_ere/bus.html"
+  },
+  {
+    "level": "5",
+    "title": "Holy Moon",
+    "difName": "Hard",
+    "artist": "ゆふ",
+    "maker": "ショウタ",
+    "scoreId": "1",
+    "url": "http://ccsyouta.sakura.ne.jp/danoni/58_holy/moon.html"
+  },
+  {
+    "level": "5",
+    "title": "Lambda Driver",
+    "difName": "NORMAL",
+    "artist": "SPELL BREAK (ETIA.)",
+    "maker": "v(゜ω゜)v",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/main/?id=Lambda&key=11"
+  },
+  {
+    "level": "5",
+    "title": "茶屋にて",
+    "difName": "Ahah↑",
+    "artist": "MATSU",
+    "maker": "apoi",
+    "scoreId": "0",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no86_teahouse.html"
+  },
+  {
+    "level": "6",
+    "title": "Last! Least! Regrets!!",
+    "difName": "Hard",
+    "artist": "ビートまりお",
+    "maker": "ふみのり",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_fuminori/?id=LLR&key=11"
+  },
+  {
+    "level": "6",
+    "title": "おうささまのしろいはだ",
+    "difName": "Very Hard",
+    "artist": "どぶウサギ",
+    "maker": "MFV2",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/main/?id=ousa&key=11"
+  },
+  {
+    "level": "6",
+    "title": "このとくべつな音色が素晴らしい",
+    "difName": "Very Hard",
+    "artist": "キノシタ",
+    "maker": "apoi",
+    "scoreId": "0",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no82_special_wonderful.html"
+  },
+  {
+    "level": "6",
+    "title": "トエト",
+    "difName": "for A",
+    "artist": "トラボルタ",
+    "maker": "apoi",
+    "scoreId": "1",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no17_toeto.html"
+  },
+  {
+    "level": "7",
+    "title": "Cross Galaxy",
+    "difName": "Galaxy",
+    "artist": "kanone",
+    "maker": "MFV2",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/main/?id=cg&key=11"
+  },
+  {
+    "level": "7",
+    "title": "G e n g a o z o",
+    "difName": "askorut",
+    "artist": "-45",
+    "maker": "askorut",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_askorut/?id=No79gen_g&key=11"
+  },
+  {
+    "level": "7",
+    "title": "Libera me",
+    "difName": "Very Hard",
+    "artist": "Cranky",
+    "maker": "MFV2",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/main/?id=LM&key=11"
+  },
+  {
+    "level": "7",
+    "title": "party 4u \"holy nite mix\"",
+    "difName": "Very Hard",
+    "artist": "Cranky",
+    "maker": "ティア",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_tia/?id=party4u&key=11"
+  },
+  {
+    "level": "7",
+    "title": "希望という命を握り締めて",
+    "difName": "Skitty",
+    "artist": "Y.W",
+    "maker": "natu",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_natu/?id=Hope"
+  },
+  {
+    "level": "8",
+    "title": "Atmosphere entry",
+    "difName": "Extreme",
+    "artist": "chip",
+    "maker": "すずめ",
+    "scoreId": "2",
+    "url": "https://suzume.kirara.st/atmosphere/?key=11"
+  },
+  {
+    "level": "8",
+    "title": "RE:RAVE",
+    "difName": "Very Hard",
+    "artist": "KURi-ZiLL",
+    "maker": "MFV2",
+    "scoreId": "1",
+    "url": "https://mfv2.sakura.ne.jp/do/main/?id=RERAVE"
+  },
+  {
+    "level": "8",
+    "title": "Volumy! (CU my DJ SIMON)",
+    "difName": "Pikopiko",
+    "artist": "Yamajet",
+    "maker": "apoi",
+    "scoreId": "0",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no18_volumy.html"
+  },
+  {
+    "level": "8",
+    "title": "サンバランド",
+    "difName": "Very Hard",
+    "artist": "SAMBA MASTER 佐藤 -SATOH-",
+    "maker": "apoi",
+    "scoreId": "2",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no42_samba_land.html"
+  },
+  {
+    "level": "8",
+    "title": "バンバード ～Piano Version～ SKY DIVE",
+    "difName": "Freedom Bard",
+    "artist": "京バンド",
+    "maker": "Bards a feather",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_bards_imodurushiki/?id=I4C8E003EB"
+  },
+  {
+    "level": "8",
+    "title": "織姫",
+    "difName": "VeryHard",
+    "artist": "DJ TAKUAN / 佐倉アヤキ",
+    "maker": "ふみのり",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_fuminori/?id=orihime&key=11"
+  },
+  {
+    "level": "9",
+    "title": "-273.15 FREEZING MAELSTROM",
+    "difName": "Absolute ZERO",
+    "artist": "U-F SEQUENCER",
+    "maker": "しん",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_Cah-m/?id=gokuaku&key=11"
+  },
+  {
+    "level": "9",
+    "title": "＋♂",
+    "difName": "Another☆２５",
+    "artist": "ギガP",
+    "maker": "フィア",
+    "scoreId": "4",
+    "url": "https://mfv2.sakura.ne.jp/do/_fia/?id=plusmale&key=11"
+  },
+  {
+    "level": "9",
+    "title": "Luv 5 Dancer",
+    "difName": "Hard",
+    "artist": "カラフル・サウンズ・ポート",
+    "maker": "Cah-m",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_Cah-m/?id=l5d&key=11"
+  },
+  {
+    "level": "9",
+    "title": "はばたけ ぼくらの バンバード",
+    "difName": "Very Bard",
+    "artist": "ゆゆうた",
+    "maker": "Bards a feather",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_bards_imodurushiki/?id=B1239D7332"
+  },
+  {
+    "level": "9",
+    "title": "闇夜之夏越祭~for 2015 edition~",
+    "difName": "Very Hard",
+    "artist": "pan",
+    "maker": "mmj x apoi",
+    "scoreId": "0",
+    "url": "http://apoi108.sakura.ne.jp/danoni/apoi/no81_yami_nagoshi.html"
+  },
+  {
+    "level": "9",
+    "title": "日傘日和",
+    "difName": "VeryHard",
+    "artist": "こむぎ",
+    "maker": "grimoldy",
+    "scoreId": "2",
+    "url": "https://mfv2.sakura.ne.jp/do/_grimoldy/?id=higasa&key=11"
+  },
+  {
+    "level": "10",
+    "title": "Big Bang Bird",
+    "difName": "Very Bard",
+    "artist": "ことり（能美孤鳥）",
+    "maker": "Bards a feather",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_bards_imodurushiki/?id=C6982F9289&key=11"
+  },
+  {
+    "level": "10",
+    "title": "Lambda Driver",
+    "difName": "ANOTHER",
+    "artist": "SPELL BREAK (ETIA.)",
+    "maker": "v(゜ω゜)v",
+    "scoreId": "1",
+    "url": "https://mfv2.sakura.ne.jp/do/main/?id=Lambda&key=11"
+  },
+  {
+    "level": "10",
+    "title": "PENETRATION",
+    "difName": "ノーザン",
+    "artist": "kinato",
+    "maker": "ノーザン",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_Northern/?id=penetration&key=11"
+  },
+  {
+    "level": "10",
+    "title": "Vanguard",
+    "difName": "Very Bard",
+    "artist": "hiyama",
+    "maker": "Bards a feather",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_bards_imodurushiki/?id=E5D363F235&key=11"
+  },
+  {
+    "level": "11",
+    "title": "Vesper",
+    "difName": "長月's Lunatic",
+    "artist": "つきいち",
+    "maker": "長月",
+    "scoreId": "2",
+    "url": "https://mfv2.sakura.ne.jp/do/_Cah-m/?id=vesper&key=11"
+  },
+  {
+    "level": "11",
+    "title": "バンバード ～Piano Version～",
+    "difName": "Very Bard",
+    "artist": "mozell",
+    "maker": "Bards a feather",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_bards_imodurushiki/?id=A22E59E2DF&key=11"
+  },
+  {
+    "level": "11",
+    "title": "白虎",
+    "difName": "超Challenge",
+    "artist": "スケッチ",
+    "maker": "YNQ",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_YNQ/?id=byakko&key=11"
+  },
+  {
+    "level": "11",
+    "title": "風の塔",
+    "difName": "Very Hard",
+    "artist": "しも",
+    "maker": "Cah-m",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_Cah-m/?id=wind"
+  },
+  {
+    "level": "12",
+    "title": "Glide",
+    "difName": "Extra",
+    "artist": "Phantasma",
+    "maker": "Ail Trap",
+    "scoreId": "3",
+    "url": "https://mfv2.sakura.ne.jp/do/_Cah-m/?id=glide&key=11"
+  },
+  {
+    "level": "12",
+    "title": "バン！バン！バンバード！！",
+    "difName": "Fire Bard",
+    "artist": "埼玉最終兵器",
+    "maker": "Bards a feather",
+    "scoreId": "0",
+    "url": "https://mfv2.sakura.ne.jp/do/_bards_imodurushiki/?id=M63047A037&key=11"
+  }
 ]
